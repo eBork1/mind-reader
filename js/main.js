258c77e0ce1f9b2ab08fb1529491b1638063aca9
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-13 18:39:04 -0400

    update main.js

---
+++ js/main.js
@@ -0,0 +1,84 @@
+var currentState = 0;
+var states = ['0', '1', '2', '3', '4', '5'];
+
+var bigText = document.querySelector('.desc');
+var smallText = document.querySelector('.ex');
+
+var nextBtn = document.querySelector('.next');
+var restartBtn = document.querySelector('.back');
+nextBtn.addEventListener('click', changeState);
+restartBtn.addEventListener('click', resetState);
+
+/* functions for changing state */
+
+function resetState() {
+    currentState = 0;
+    renderState();
+}
+
+function changeState() {
+    currentState++;
+    renderState();
+}
+
+console.log(states[currentState]);
+/*  render the pages/states */
+
+function renderState() {
+
+    switch (states[currentState]) {
+
+        case '0': 
+            bigText.textContent = "I can read your mind";
+            smallText.textContent = "Press next to continue";
+            restartBtn.style.visibility = 'hidden';
+            break;
+
+        case '1': 
+            bigText.textContent = "Pick a number between 1-99";
+            smallText.textContent = "When you have picked your number, click next";
+            break;
+
+        case '2':
+            bigText.textContent = "Add both digits together to get a new number";
+            smallText.textContent = "Ex: 14 is 1 + 4 = 5";
+            break;
+
+        case '3':
+            bigText.textContent = "Subtract your new number from the original number";
+            smallText.textContent = "Ex: 14 - 5 = 9";
+            break;
+        
+        case '4':
+
+            break;
+        
+        case '5':
+
+            break; 
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
